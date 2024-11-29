3ba4a009476575a9a28ff8fbe21c1928e12bc29e
appu883 <appusrinivas883@gmail.com>
2024-11-29 08:34:30 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Sample App!');
+});
+
+app.listen(port, () => {
+  console.log(`Sample app listening at http://localhost:${port}`);
+});
