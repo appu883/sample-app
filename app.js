add252dd8bb19c81fd4b6d5e70690aa3c6ebe12f
appu883 <appusrinivas883@gmail.com>
2024-11-29 10:38:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Sample App!');
+  res.send('Hello from Sample App');
 });
 
 app.listen(port, () => {
